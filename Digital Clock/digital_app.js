d224fb478498d2da0c00f59b809cd62975a0874c
Christine <74520672+chrish8510@users.noreply.github.com>
2021-05-04 22:30:39 -0400

    Update digital_app.js

--- Digital Clock/digital_app.js
+++ Digital Clock/digital_app.js
@@ -1,22 +1,38 @@
 function currentTime() {
-    let date = new Date(); /* creating object of Date class */
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
-      let t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
-  }
+  let date = new Date(); /* creating object of Date class */
+  let hour = date.getHours(); /* using the methods of Date object get "hours", "minutes" and "seconds" */
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  /* adding AM or PM next to time */
+  am_pm = "AM";
   
-  function updateTime(s) {
-    if (s < 10) {
-      return "0" + s;
-    }
-    else {
-      return s;
-    }
+  /* function for the AM,PM */  
+  if (hour > 12) {
+      hour -= 12;
+      am_pm = "PM";
   }
-  
-  currentTime(); /* calling currentTime() function to initiate the process */
+
+  if (hour == 0) {
+      hr = 12;
+      am_pm = "AM";
+  }
+
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + am_pm; /* adding time to the div */
+}
+
+
+
+function updateTime(t) {
+  if (t < 10) {
+    return "0" + t;
+  }
+  else {
+    return t;
+  }
+
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
