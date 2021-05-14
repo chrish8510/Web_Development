f4950f058a6d68c0f9377014990cb6dc1cd3d358
Christine <74520672+chrish8510@users.noreply.github.com>
2021-05-14 05:42:02 -0400

    Update Trip Planner.js

--- SoloLearn Projects/Trip Planner.js
+++ SoloLearn Projects/Trip Planner.js
@@ -1,6 +1,9 @@
 function main() {
+    // parseInt is converts its first argument to a string, pconverts that string, then returns an integer 
     let distance = parseInt(readLine(), 10);
+    // distance in miles as input
     let result = distance/40;
+    //output to the console - the time it will take you to cover it in minutes
     let solution = result*60;
     console.log(solution);
 }  
